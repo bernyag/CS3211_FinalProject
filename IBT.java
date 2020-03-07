65374fb44db37b3897da3ffb2cdb61780742a03b
RACHIT BANSAL <rachit@cse.iitb.ac.in>
2020-03-08 03:05:28 +0800

    Update IBT.java

--- IBT.java
+++ IBT.java
@@ -4,17 +4,24 @@ public class IBT extends Thread{
 	public IBT( List<URLHTMLTuple> BUL){
 		this.BUL=BUL;
 	}
-	public void run(){
-		if(BUL.size() ==  1000){
-			
-			
+	public void transfer(List<URLHTMLTuple> BUL){
+		for (URLHTMLTuple temp : BUL){
+			//synchronization needed
+			if(!IUT.search(temp)){//IUT is the global URLtree
+				IUT.insert(temp);
+			}
+			//synchronization end
 		}
-
 	}
 
-
-
-	
+	public void run(){
+		while(true){
+			if(BUL.size() ==  1000){
+			transfer(BUL);
+			BUL.clear();
+		}
+	}
+	}
 }
 
 
