6a5636fcba55f7c377ea6f7595c8bd1cb2c764f6
RACHIT BANSAL <rachit@cse.iitb.ac.in>
2020-03-07 18:22:34 +0800

    Create IBT.java

---
+++ IBT.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class IBT extends Thread{
+	private List<URLHTMLTuple> BUL;
+	public IBT( List<URLHTMLTuple> BUL){
+		this.BUL=BUL;
+	}
+	int size = list.size();
+
+
+	
+}
+
+
